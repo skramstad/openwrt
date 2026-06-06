47b75b7829ab6eb21c00d2aded0e37b448aab342
Kristian Skramstad <kristian+github@83.no>
2026-06-06 07:05:16 +0200

    mtk: add txpower per radio.
    Create 700-nl80211-freqlist-per-radio.patch

--- package/network/config/wifi-scripts/files-ucode/lib/netifd/wireless/mac80211.sh
+++ package/network/config/wifi-scripts/files-ucode/lib/netifd/wireless/mac80211.sh
@@ -106,7 +106,7 @@ function setup_phy(phy, config, data) {
 		system(`iw phy ${phy} set antenna ${config.txantenna} ${config.rxantenna} >/dev/null 2>&1`);
 	}
 	system(`iw phy ${phy} set distance ${config.distance} >/dev/null 2>&1`);
-	system(`iw phy ${phy} set txpower ${config.txpower}`);
+	system(`iw phy ${phy} set txpower ${config.txpower} radio ${config.radio}`);
 
 	if (config.frag)
 		system(`iw phy ${phy} set frag ${config.frag}`);
